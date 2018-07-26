c058f0c25368a087b8055d9f163107ac23f0b9e4
Michael Bloom <MikeBloom914@gmail.com>
2018-07-26 19:33:44 -0400

    Update

--- table.py
+++ table.py
@@ -14,10 +14,10 @@ app = dash.Dash()
 app.scripts.config.serve_locally = True
 # app.css.config.serve_locally = True
 
-DF_ChartBit = pd.read_csv('newchart.csv')
+DF_TableBit = pd.read_csv('newchart.csv')
 
-DF_ChartBit = DF_ChartBit[DF_ChartBit['Term'] == '05/01/17-04/30/18']
-DF_ChartBit.loc[0:20]
+DF_TableBit = DF_TableBit[DF_TableBit['Term'] == '05/01/17-04/30/18']
+DF_TableBit.loc[0:20]
 
 DF_SIMPLE = pd.DataFrame({
     'x': ['A', 'B', 'C', 'D', 'E', 'F'],
@@ -36,12 +36,12 @@ ROWS = [
 
 
 app.layout = html.Div([
-    html.H4('Cryptos Abyss'),
+    html.H4('CryptosAbyss'),
     dt.DataTable(
-        rows=DF_ChartBit.to_dict('records'),
+        rows=DF_TableBit.to_dict('records'),
 
         # optional - sets the order of columns
-        # columns=sorted(DF_ChartBit.columns),
+        # columns=sorted(DF_TableBit.columns),
 
         row_selectable=True,
         filterable=True,
@@ -78,8 +78,8 @@ def update_figure(rows, selected_row_indices):
     dff = pd.DataFrame(rows)
     fig = plotly.tools.make_subplots(
         rows=3, cols=1,
-        subplot_titles=('Correlation', 'Correlation one week ahead', 'Correlation one month ahead',),
-        shared_xaxes=True)
+        subplot_titles=('Correlation to Price of Bitcoin', 'Correlation one week ahead of the price of the Bitcoin', 'Correlation one month ahead of the price of Bitcoin',),
+        shared_xaxes=False)
     marker = {'color': ['#0074D9'] * len(dff)}
     for i in (selected_row_indices or []):
         marker['color'][i] = '#FF851B'
@@ -102,14 +102,14 @@ def update_figure(rows, selected_row_indices):
         'marker': marker
     }, 3, 1)
     fig['layout']['showlegend'] = False
-    fig['layout']['height'] = 800
+    fig['layout']['height'] = 1400
     fig['layout']['margin'] = {
         'l': 40,
         'r': 10,
         't': 60,
         'b': 200
     }
-    fig['layout']['yaxis2']['type'] = 'log'
+    fig['layout']['yaxis4']['type'] = 'log'
     return fig
 
 
